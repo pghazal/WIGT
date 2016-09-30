2740a8aaf24f0e2ae768758e98b8410826531530
pghazal <pghazal@omnilog.fr>
2016-09-30 10:02:49 +0200

    Create activity package

--- app/src/main/java/com/punchlag/wigt/activity/MainActivity.java
+++ app/src/main/java/com/punchlag/wigt/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.punchlag.wigt;
+package com.punchlag.wigt.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.punchlag.wigt.R;
 
 public class MainActivity extends AppCompatActivity {
 
