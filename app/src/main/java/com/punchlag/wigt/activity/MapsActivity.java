c786c185f76a4a8d1b743e661829583a4a8e3106
pghazal <pghazal@omnilog.fr>
2016-10-07 17:52:54 +0200

    Create a MapsFragment which will handle all Google maps logic

--- app/src/main/java/com/punchlag/wigt/activity/MapsActivity.java
+++ app/src/main/java/com/punchlag/wigt/activity/MapsActivity.java
@@ -3,19 +3,11 @@ package com.punchlag.wigt.activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.punchlag.wigt.R;
+import com.punchlag.wigt.fragment.MapsFragment;
 import com.punchlag.wigt.utils.Arguments;
 
-public class MapsActivity extends BaseActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
+public class MapsActivity extends BaseActivity {
 
     private boolean mAlarmActivated;
 
@@ -33,17 +25,13 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback {
         } else {
             parseArguments(savedInstanceState);
         }
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        addActivityContentFragmentWithTag(MapsFragment.newInstance(), MapsFragment.FRAGMENT_TAG);
     }
 
     private void parseArguments(Bundle args) {
         if (args != null) {
             mAlarmActivated = args.getBoolean(Arguments.ARG_ALARM_ACTIVATED);
         }
-
-        Log.d("TAG", "" + mAlarmActivated);
     }
 
     @Override
@@ -54,23 +42,5 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
---
+++ app/src/main/java/com/punchlag/wigt/fragment/MapsFragment.java
@@ -0,0 +1,104 @@
+package com.punchlag.wigt.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.BindView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.punchlag.wigt.R;
+
+public class MapsFragment extends BaseFragment implements OnMapReadyCallback {
+
+    public static final String FRAGMENT_TAG = MapsFragment.class.getSimpleName();
+
+    @BindView(R.id.mapView)
+    MapView mapView;
+
+    public static MapsFragment newInstance() {
+        MapsFragment mapFragment = new MapsFragment();
+        Bundle args = new Bundle();
+        mapFragment.setArguments(args);
+        return mapFragment;
+    }
+
+    @Override
+    public int getLayoutResourceId() {
+        return R.layout.fragment_maps;
+    }
+
+    public void configureSubviews(Bundle savedInstanceState) {
+        initMapView(savedInstanceState);
+    }
+
+    private void initMapView(Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
