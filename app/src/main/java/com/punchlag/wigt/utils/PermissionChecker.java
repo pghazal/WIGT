54788a4a1d9d50de87c97326f1c3d659dd3968df
Pierre Ghazal <pghazal-ext@lequipe.fr>
2016-10-08 14:40:59 +0200

    Remove Coarse location because Fine location is sufficient

--- app/src/main/java/com/punchlag/wigt/utils/PermissionChecker.java
+++ app/src/main/java/com/punchlag/wigt/utils/PermissionChecker.java
@@ -12,21 +12,17 @@ import java.util.Arrays;
 public class PermissionChecker {
 
     private static final String[] locationPermissionsList = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
 
     public static boolean hasLocationPermissionGranted(Context context) {
         return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED;
     }
 
     public static boolean hasLocationPermissionResultGranted(@NonNull String[] permissions, @NonNull int[] grantResults) {
         if (grantResults.length > 0 && Arrays.equals(locationPermissionsList, permissions)
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
 
