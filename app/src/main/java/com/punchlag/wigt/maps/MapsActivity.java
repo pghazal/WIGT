212649da6387bd7e2283d601e2aa2b47d3cf0bf3
Pierre Ghazal <pghazal-ext@lequipe.fr>
2016-10-27 21:31:48 +0200

    Remove useless menu

--- app/src/main/java/com/punchlag/wigt/maps/MapsActivity.java
+++ app/src/main/java/com/punchlag/wigt/maps/MapsActivity.java
@@ -48,27 +48,6 @@ public class MapsActivity extends BaseActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_maps, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            case R.id.action_switch_view:
-                isMapModeEnabled = !isMapModeEnabled;
-                switchDisplayMode(isMapModeEnabled);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void switchDisplayMode(boolean isMapModeEnabled) {
         if (isMapModeEnabled) {
             MapsFragment fragment = (MapsFragment) getSupportFragmentManager().findFragmentByTag(MapsFragment.FRAGMENT_TAG);
