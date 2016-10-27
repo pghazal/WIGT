665cb79ae74e3d6138084e8a0f24bc014b1b2ce0
Pierre Ghazal <pghazal-ext@lequipe.fr>
2016-10-27 21:30:35 +0200

    Add switch for display view mode

--- app/src/main/java/com/punchlag/wigt/maps/MapsActivity.java
+++ app/src/main/java/com/punchlag/wigt/maps/MapsActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 
 import com.punchlag.wigt.R;
 import com.punchlag.wigt.activity.BaseActivity;
 
+import butterknife.BindView;
+
 public class MapsActivity extends BaseActivity {
+
+    @BindView(R.id.switchRadioGroup)
+    RadioGroup switchRadioGroup;
 
     private boolean isMapModeEnabled = true;
 
@@ -26,6 +32,14 @@ public class MapsActivity extends BaseActivity {
         } else {
             parseArguments(savedInstanceState);
         }
+
+        switchRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                isMapModeEnabled = !isMapModeEnabled;
+                switchDisplayMode(isMapModeEnabled);
+            }
+        });
 
         switchDisplayMode(isMapModeEnabled);
     }
